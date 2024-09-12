832399d2bc4fb52efb09fddc9451e9ca0057c961
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-09-12 02:14:24 -0300

    Creating the EmailEntity

---
+++ src/main/java/br/com/empiricus/auth_email_service/emails/adapter/outbound/entity/EmailEntity.java
@@ -0,0 +1,43 @@
+package br.com.empiricus.auth_email_service.emails.adapter.outbound.entity;
+
+import br.com.empiricus.auth_email_service.emails.core.domain.Email;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "emails")
+public class EmailEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Column(unique = true)
+    @jakarta.validation.constraints.Email
+    private String email;
+
+    @Column(name = "data_criacao")
+    private String creationDate;
+
+    @Column(name = "data_atualizacao")
+    private String updateDate;
+
+    @CPF
+    @NotBlank
+    private String cpf;
+
+    @Column(name = "eh_admin")
+    private boolean admin;
+
+    public Email toEmail() {
+        return new Email(this.id, this.cpf, this.email, this.creationDate, this.updateDate, this.admin);
+    }
+}
