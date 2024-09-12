646604de64155f2b0ead47175c0a26fd3663d899
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-09-12 01:07:26 -0300

    Implementing the necessary configuration for SaveUserService

--- src/main/java/br/com/empiricus/auth_email_service/users/configuration/BeanConfiguration.java
+++ src/main/java/br/com/empiricus/auth_email_service/users/configuration/BeanConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.empiricus.auth_email_service.users.configuration;
 
 import br.com.empiricus.auth_email_service.users.core.ports.outbound.FindUserRepositoryPort;
-import br.com.empiricus.auth_email_service.users.core.service.FindUserService;
+import br.com.empiricus.auth_email_service.users.core.ports.outbound.SaveUserRepositoryPort;
+import br.com.empiricus.auth_email_service.users.core.services.FindUserService;
+import br.com.empiricus.auth_email_service.users.core.services.SaveUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,10 @@ public class BeanConfiguration {
     @Bean
     public FindUserService findUserService(FindUserRepositoryPort findUserRepository) {
         return new FindUserService(findUserRepository);
+    }
+
+    @Bean
+    public SaveUserService saveUserService(SaveUserRepositoryPort saveUserRepository) {
+        return new SaveUserService(saveUserRepository);
     }
 }
