2a45db332235d56aa1d05b37b51b1b78f2922af2
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-09-12 01:37:22 -0300

    Renaming file

--- src/main/java/br/com/empiricus/auth_email_service/users/configuration/UserBeanConfiguration.java
+++ src/main/java/br/com/empiricus/auth_email_service/users/configuration/UserBeanConfiguration.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class BeanConfiguration {
+public class UserBeanConfiguration {
 
     @Bean
     public FindUserService findUserService(FindUserRepositoryPort findUserRepository) {
