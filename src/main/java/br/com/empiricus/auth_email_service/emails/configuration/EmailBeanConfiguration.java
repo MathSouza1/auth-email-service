73dcc571bdf35d5cccddcd289cfe61b2f2628d68
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-09-12 03:28:20 -0300

    Implementing the necessary configuration for DeleteEmailService

--- src/main/java/br/com/empiricus/auth_email_service/emails/configuration/EmailBeanConfiguration.java
+++ src/main/java/br/com/empiricus/auth_email_service/emails/configuration/EmailBeanConfiguration.java
@@ -1,7 +1,9 @@
 package br.com.empiricus.auth_email_service.emails.configuration;
 
+import br.com.empiricus.auth_email_service.emails.core.ports.outbound.DeleteEmailRepositoryPort;
 import br.com.empiricus.auth_email_service.emails.core.ports.outbound.FindEmailRepositoryPort;
 import br.com.empiricus.auth_email_service.emails.core.ports.outbound.SaveEmailRepositoryPort;
+import br.com.empiricus.auth_email_service.emails.core.services.DeleteEmailService;
 import br.com.empiricus.auth_email_service.emails.core.services.FindEmailService;
 import br.com.empiricus.auth_email_service.emails.core.services.SaveEmailService;
 import br.com.empiricus.auth_email_service.users.core.ports.outbound.FindUserRepositoryPort;
@@ -19,5 +21,10 @@ public class EmailBeanConfiguration {
     @Bean
     public SaveEmailService saveEmailService(SaveEmailRepositoryPort saveEmailRepository, FindUserRepositoryPort findUserRepositoryPort) {
         return new SaveEmailService(saveEmailRepository, findUserRepositoryPort);
+    }
+
+    @Bean
+    public DeleteEmailService deleteEmailService(DeleteEmailRepositoryPort deleteEmailRepository, FindUserRepositoryPort findUserRepositoryPort) {
+        return new DeleteEmailService(deleteEmailRepository, findUserRepositoryPort);
     }
 }
