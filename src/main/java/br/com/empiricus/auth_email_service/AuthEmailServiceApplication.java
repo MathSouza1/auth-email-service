f10822e2e065da96753238212c98d97a2b5e1897
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-09-11 23:03:08 -0300

    First commit

---
+++ src/main/java/br/com/empiricus/auth_email_service/AuthEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.empiricus.auth_email_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthEmailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empiricus/auth_email_service/AuthEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empiricus.auth_email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
